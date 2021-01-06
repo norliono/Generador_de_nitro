c415f48e060ade3006755156de56dfe2fe7809fa
norliono <77075950+norliono@users.noreply.github.com>
2021-01-06 17:08:44 -0300

    Add files via upload

---
+++ Nitro_Gen/Generador (BETA).bat
@@ -0,0 +1,48 @@
+@echo off
+ECHO.
+Rem -------------------Variables------------------------
+SET Nitro_Code1=https://discord.gift/
+SET Nitro_Version=1.0.5
+SET Titulo=Generador de nitro Version %Nitro_Version%
+color 1A
+Rem -------------------Codigo----------------------------
+title %Titulo%
+Cls
+cls
+Echo Cargando.
+Ping localhost -n 2.40 >nul
+cls
+
+Echo Cargando..
+Ping localhost -n 2.40 >nul
+cls
+Echo Cargando...
+Ping localhost -n 2.40 >nul
+cls
+Echo Cargando.
+Ping localhost -n 2.40 >nul
+cls
+Echo Cargando..
+Ping localhost -n 2.40 >nul
+cls
+Echo Cargando...
+Ping localhost -n 2.40 >nul
+cls
+Echo Cargando.
+Ping localhost -n 2.40 >nul
+cls
+Echo Cargando..
+Ping localhost -n 2.40 >nul
+cls
+Echo Cargando...
+Ping localhost -n 2.40 >nul
+cls
+
+Echo Empezar a generar?
+pause
+cls
+msg * generador hecho por norliono#5793 , todos los derechos reservados
+:Gen
+echo %Nitro_Code1%%random%%random%%random%
+Ping localhost -n 1.30 >nul
+goto :Gen
